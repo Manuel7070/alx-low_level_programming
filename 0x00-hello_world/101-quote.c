cdca9be69c885ac9c531e1a84a6d940fbeb7f78e
Manuel7070 <rubangakene3030ec@gmail.com>
2023-07-17 13:01:00 -0700

    UNIX is basically simple operating machine

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - C programme which print a line to std error
  *
- * Return 1 for succuess
+ * Return: 1 for succuess
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\n", stdout);
+	fputs("  - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
-}	
+}
