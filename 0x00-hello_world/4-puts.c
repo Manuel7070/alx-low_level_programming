3fe98a93bc1ea5b9d9756ac01e3792a211b9b6c4
Manuel7070 <rubangakene3030ec@gmail.com>
2023-07-17 10:23:45 -0700

    My first executed programme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - My function code
+ *
+ * Return: alaways 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
