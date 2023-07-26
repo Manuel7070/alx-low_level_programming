af54e438ef6628196e86cb166352b2642750166e
Manuel7070 <rubangakene3030ec@gmail.com>
2023-07-26 10:14:07 -0700

    program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
- * print_numbers - prints numbers
- * @n: integers
- * Return: 0 always *
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
 /*
@@ -20,11 +19,9 @@ p = &n;
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
-* - Retrun: 0 always
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-~
